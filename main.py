382d47b26bec8122e0a4b429db08e2ed4f25ca73
Efrain Rosas <carlos.rosas7476@alumnos.udg.mx>
2021-08-07 19:13:19 -0500

    Implementacion de funcion cambio_divisas

--- main.py
+++ main.py
@@ -13,71 +13,42 @@ def menu_divisa(divisa, peso):
     print("1.- Convertir de pesos " + peso + " a dolares americanos")
     print("2.- Convertir de dolares americanos a pesos " + peso)
 
+def cambio_divisas(peso):
+    opcion = int(input("Opcion: "))
+    if opcion == 1: 
+        pesos = input("Cuantos pesos " + peso + " tienes: ")
+        pesos = float(pesos)
+        valor_dolar = float(input("Tipo de cambio: 1 dolar equivale a: "))
+        dolares = pesos / valor_dolar
+        dolares = round(dolares, 2)
+        dolares = str(dolares)
+        print("Tienes $ " + dolares + " Dolares")
+    elif(opcion == 2):
+        dolares = input("Cuantos dolares americanos tienes: ")
+        dolares = float(dolares)
+        valor_dolar = float(input("Tipo de cambio: 1 dolar equivale a: "))
+        pesos = dolares * valor_dolar
+        pesos = round(pesos, 2)
+        pesos = str(pesos)
+        print("Tienes $ " + pesos + " Pesos " + peso + ".")
+    else: 
+        print("Opcion ingresada no valida, por favor verifique si respuesta, gracias ...")
+
+
 print(menu, end = "")
 opcion = int(input("Opcion: "))
 
 if opcion == 1: 
     menu_divisa("MEXICANA", "mexicanos")
-    opcion = int(input("Opcion: "))
-    if opcion == 1: 
-        pesos = input("Cuantos pesos mexicanos tienes: ")
-        pesos = float(pesos)
-        valor_dolar = float(input("Tipo de cambio: 1 dolar equivale a: "))
-        dolares = pesos / valor_dolar
-        dolares = round(dolares, 2)
-        dolares = str(dolares)
-        print("Tienes $" + dolares + " Dolares")
-    elif(opcion == 2):
-        dolares = input("Cuantos dolares americanos tienes: ")
-        dolares = float(dolares)
-        valor_dolar = float(input("Tipo de cambio: 1 dolar equivale a: "))
-        pesos = dolares * valor_dolar
-        pesos = round(pesos, 2)
-        pesos = str(pesos)
-        print("Tienes $" + pesos + " Pesos mexicanos")
-    else: 
-        print("Opcion ingresada no valida, por favor verifique si respuesta, gracias ...")
+    cambio_divisas("mexicanos")
+    
 elif opcion == 2: 
     menu_divisa("COLOMBIANA", "colombianos")
-    opcion = int(input("Opcion: "))
-    if opcion == 1: 
-        pesos = input("Cuantos pesos colombianos tienes: ")
-        pesos = float(pesos)
-        valor_dolar = float(input("Tipo de cambio: 1 dolar equivale a: "))
-        dolares = pesos / valor_dolar
-        dolares = round(dolares, 2)
-        dolares = str(dolares)
-        print("Tienes $" + dolares + " Dolares")
-    elif(opcion == 2):
-        dolares = input("Cuantos dolares americanos tienes: ")
-        dolares = float(dolares)
-        valor_dolar = float(input("Tipo de cambio: 1 dolar equivale a: "))
-        pesos = dolares * valor_dolar
-        pesos = round(pesos, 2)
-        pesos = str(pesos)
-        print("Tienes $" + pesos + " Pesos colombianos")
-    else: 
-        print("Opcion ingresada no valida, por favor verifique si respuesta, gracias ...")
+    cambio_divisas("colombianos")
+
 elif opcion == 3: 
     menu_divisa("ARGENTINA", "argentinos")
-    opcion = int(input("Opcion: "))
-    if opcion == 1: 
-        pesos = input("Cuantos pesos argentinos tienes: ")
-        pesos = float(pesos)
-        valor_dolar = float(input("Tipo de cambio: 1 dolar equivale a: "))
-        dolares = pesos / valor_dolar
-        dolares = round(dolares, 2)
-        dolares = str(dolares)
-        print("Tienes $" + dolares + " Dolares")
-    elif(opcion == 2):
-        dolares = input("Cuantos dolares americanos tienes: ")
-        dolares = float(dolares)
-        valor_dolar = float(input("Tipo de cambio: 1 dolar equivale a: "))
-        pesos = dolares * valor_dolar
-        pesos = round(pesos, 2)
-        pesos = str(pesos)
-        print("Tienes $" + pesos + " Pesos argentinos")
-    else: 
-        print("Opcion ingresada no valida, por favor verifique si respuesta, gracias ...")
+    cambio_divisas("argentinos")
+
 else:
     print("Opcion ingresada no valida, por favor verifique si respuesta, gracias ...")    
